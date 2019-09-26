9df260a287f484c1d071d6023d6457fd2de3914b
Omair-byte <Iso.1290>
2019-09-26 15:34:45 +0100

    commit

--- Week 2/IntegerToBinary.py
+++ Week 2/IntegerToBinary.py
@@ -1,12 +1,16 @@
 import math
 integer=int(input("Input any integer number :"))
 
-binary=  ''
+binary= ""
+def reverse(s):
+    for i in range(len(s),0,-1):
+        reverse = reverse + str (s[i-1])
+    return reverse
 
 while integer !=0:
     binary = binary + str(integer % 2)
     integer = int(integer /2)
 
-print("The binary is: ",binary)
 
+print("The binary is: ",reverse)
 
